34e80e2962ab71b0cee36ab1004621a7ee626ff2
Steve Desmond <steve@vtsv.ca>
2020-07-17 13:06:13 -0400

    Add LinuxSignalReader

---
+++ test/LinuxSignalReaderTests.cs
@@ -0,0 +1,70 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using NSubstitute;
+using Xunit;
+
+namespace WiFiHeatMap.Tests
+{
+    public class LinuxSignalReaderTests
+    {
+        [Fact]
+        public async Task ReturnsOutputFromProcess()
+        {
+            //arrange
+            var commandService = Substitute.For<ICommandService>();
+            commandService.Run(Arg.Any<ProcessStartInfo>()).Returns("file contents");
+            var reader = new LinuxSignalReader(commandService);
+
+            //act
+            var results = await reader.Read();
+
+            //assert
+            Assert.Equal("file contents", results);
+        }
+
+        [Fact]
+        public async Task ReturnsDecentMessageWhenNotInstalled()
+        {
+            //arrange
+            var exception = new ExternalException("x", 2);
+            var commandService = Substitute.For<ICommandService>();
+            commandService.When(c => c.Run(Arg.Any<ProcessStartInfo>())).Throw(exception);
+            var reader = new LinuxSignalReader(commandService);
+
+            try
+            {
+                //act
+                var results = await reader.Read();
+            }
+            catch (Exception e)
+            {
+                //assert
+                Assert.Contains("\"network-tools\" is installed", e.Message);
+            }
+        }
+
+        [Fact]
+        public async Task ReturnsDecentMessageWhenNotRunningAsRoot()
+        {
+            //arrange
+            var exception = new ExternalException("x", 13);
+            var commandService = Substitute.For<ICommandService>();
+            commandService.When(c => c.Run(Arg.Any<ProcessStartInfo>())).Throw(exception);
+            var reader = new LinuxSignalReader(commandService);
+
+            try
+            {
+                //act
+                var results = await reader.Read();
+            }
+            catch (Exception e)
+            {
+                //assert
+                Assert.Contains("run as root", e.Message);
+            }
+        }
+    }
+}
