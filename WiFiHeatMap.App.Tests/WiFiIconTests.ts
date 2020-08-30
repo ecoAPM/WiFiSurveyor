826e88bcf1576ada09fcef935d1f4fa530048c32
Steve Desmond <steve@vtsv.ca>
2020-08-30 15:21:12 -0400

    Add Wi-Fi icon component

---
+++ WiFiHeatMap.App.Tests/WiFiIconTests.ts
@@ -0,0 +1,18 @@
+import { Test, TestSuite } from "xunit.ts";
+import { shallowMount as mount } from '@vue/test-utils';
+import WiFiIcon from '../WiFiHeatMap.App/wifi-icon.vue';
+
+export default class WiFiIconTests extends TestSuite {
+    @Test()
+    async colorGetsPassedToSVG()
+    {
+        //arrange
+        const color = 'rgba(0, 255, 0, 1)';
+
+        //act
+        const component = mount(WiFiIcon, { propsData: { color: color}});
+
+        //assert
+        this.assert.stringContains('style="fill: rgba(0, 255, 0, 1);"', component.html());
+    }
+}
