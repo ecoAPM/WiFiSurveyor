4dafb45b079a18398eb633f7c8fea558636dee40
Steve Desmond <steve@vtsv.ca>
2020-09-04 15:24:04 -0400

    Add connection status component

---
+++ WiFiHeatMap.App.Tests/StatusTests.ts
@@ -0,0 +1,31 @@
+import { Test, TestSuite } from "xunit.ts";
+import Status from '../WiFiHeatMap.App/status.vue';
+import { shallowMount as mount } from '@vue/test-utils';
+
+export default class StatusTests extends TestSuite {
+    @Test()
+    async showsStatusWhenOneExists()
+    {
+        //arrange
+        const status = 'test message';
+
+        //act
+        const component = mount(Status, {propsData: {status: status}});
+
+        //assert
+        this.assert.equal('test message', component.text());
+    }
+
+    @Test()
+    async componentDoesNotDisplayWhenNoStatus()
+    {
+        //arrange
+        const status = '';
+
+        //act
+        const component = mount(Status, {propsData: {status: status}});
+
+        //assert
+        this.assert.empty(component.html());
+    }
+}
---
+++ WiFiHeatMap.App/status.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="status" v-if="status">
+    <pre>{{status}}</pre>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  export default Vue.extend({
+    props: {
+      status: String
+    }
+  });
+</script>
+
+<style scoped>
+  .status {
+    position: absolute;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, 0.25);
+  }
+
+  pre {
+    padding: 1rem;
+    background: white;
+    border: 2px solid black;
+    border-radius: 8px;
+    box-shadow: 0 0 1rem 1rem rgba(0, 0, 0, 0.5);
+  }
+</style>
