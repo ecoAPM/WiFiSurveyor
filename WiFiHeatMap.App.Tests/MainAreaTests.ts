45bb5e7da13d74e14796a69776d80b887f921d48
Steve Desmond <steve@vtsv.ca>
2020-09-03 20:46:06 -0400

    Add component for main interaction area

---
+++ WiFiHeatMap.App.Tests/MainAreaTests.ts
@@ -0,0 +1,90 @@
+import { Test, TestSuite } from "xunit.ts";
+import MainArea from '../WiFiHeatMap.App/main-area.vue';
+import { shallowMount as mount } from '@vue/test-utils';
+import Reading from "../WiFiHeatMap.App/Reading";
+import Point from "../WiFiHeatMap.App/Point";
+import Mockito from 'ts-mockito';
+import Renderer from "../WiFiHeatMap.App/Renderer";
+import MockFactory from "./MockFactory";
+
+export default class MainAreaTests extends TestSuite {
+    @Test()
+    async showsDataPointForEachReading() {
+        //arrange
+        const gl = MockFactory.WebGL2RenderingContext();
+        const canvas = Mockito.mock<HTMLCanvasElement>();
+        Mockito.when(canvas.getContext('webgl2')).thenReturn(Mockito.instance(gl));
+        const readings: Reading[] = [
+            new Reading(1, new Point(2, 3), []),
+            new Reading(2, new Point(3, 4), [])
+        ];
+
+        //act
+        const component = mount(MainArea, { propsData: { readings: readings, current: new Reading(0, new Point(0, 0), []), renderer: new Renderer(Mockito.instance(canvas)) } });
+
+        //assert
+        this.assert.equal(2, component.findAll('data-point-stub').length);
+    }
+
+    @Test()
+    async backgroundIsSetFromData()
+    {
+        const gl = MockFactory.WebGL2RenderingContext();
+        const canvas = Mockito.mock<HTMLCanvasElement>();
+        Mockito.when(canvas.getContext('webgl2')).thenReturn(Mockito.instance(gl));
+        const readings: Reading[] = [];
+
+        //act
+        const component = mount(MainArea, { propsData: { readings: readings, current: new Reading(0, new Point(0, 0), []), renderer: new Renderer(Mockito.instance(canvas)), background: 'test.png' } });
+
+        //assert
+        this.assert.stringContains('background-image: url(test.png);', component.find('.background').attributes('style'));
+    }
+
+    @Test()
+    async backgroundIsPixelatedWhenFlagSet()
+    {
+        const gl = MockFactory.WebGL2RenderingContext();
+        const canvas = Mockito.mock<HTMLCanvasElement>();
+        Mockito.when(canvas.getContext('webgl2')).thenReturn(Mockito.instance(gl));
+        const readings: Reading[] = [];
+
+        //act
+        const component = mount(MainArea, { propsData: { readings: readings, current: new Reading(0, new Point(0, 0), []), renderer: new Renderer(Mockito.instance(canvas)), pixelated: true } });
+
+        //assert
+        this.assert.stringContains('pixelated', component.find('.background').attributes('class'));
+    }
+
+    @Test()
+    async backgroundIsNotPixelatedWhenFlagNotSet()
+    {
+        const gl = MockFactory.WebGL2RenderingContext();
+        const canvas = Mockito.mock<HTMLCanvasElement>();
+        Mockito.when(canvas.getContext('webgl2')).thenReturn(Mockito.instance(gl));
+        const readings: Reading[] = [];
+
+        //act
+        const component = mount(MainArea, { propsData: { readings: readings, current: new Reading(0, new Point(0, 0), []), renderer: new Renderer(Mockito.instance(canvas)), pixelated: false } });
+
+        //assert
+        this.assert.stringDoesNotContain('pixelated', component.find('.background').attributes('class'));
+    }
+    
+    @Test()
+    async clickingCanvasAddsReading() {
+        //arrange
+        const gl = MockFactory.WebGL2RenderingContext();
+        const canvas = Mockito.mock<HTMLCanvasElement>();
+        Mockito.when(canvas.getContext('webgl2')).thenReturn(Mockito.instance(gl));
+        const readings: Reading[] = [];
+        const component = mount(MainArea, { propsData: { readings: readings, current: new Reading(0, new Point(0, 0), []), renderer: new Renderer(Mockito.instance(canvas)) } });
+
+        //act
+        await component.find('canvas').trigger('click');
+        await component.vm.$nextTick();
+
+        //assert
+        this.assert.equal(1, readings.length);
+    }
+}
---
+++ WiFiHeatMap.App/main-area.vue
@@ -0,0 +1,79 @@
+<template>
+  <main>
+    <div class="background" :style="{'background-image': `url(${background})`}" :class="{'pixelated': pixelated}"></div>
+    <canvas id="webglcanvas" @click="add($event)"></canvas>
+    <data-point v-for="(reading, index) in readings" :key="index" :reading="reading" :selected="selected"></data-point>
+  </main>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import DataPoint from './data-point.vue';
+  import Renderer from './Renderer';
+  import Point from './Point';
+  import Reading from './Reading';
+  import AccessPoint from './AccessPoint';
+
+  export default Vue.extend({
+    components: {
+      'data-point': DataPoint
+    },
+    props: {
+      renderer: Renderer,
+      background: String,
+      pixelated: Boolean,
+      readings: Array,
+      current: Reading,
+      selected: AccessPoint
+    },
+    mounted(): void {
+      const canvas = document.getElementById("webglcanvas") as HTMLCanvasElement;
+      if (canvas != null) {
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+      }
+      window.addEventListener('resize', this.refresh);
+    },
+    methods: {
+      add(e: MouseEvent): void {
+        const location = new Point(e.offsetX, e.offsetY);
+        const reading = new Reading(this.readings.length, location, this.current.signals.concat());
+        this.readings.push(reading);
+        this.refresh();
+      },
+      refresh(): void {
+        this.renderer.render(this.readings, this.selected);
+      }
+    }
+  });
+</script>
+
+<style>
+  main {
+    flex-grow: 1;
+    position: relative;
+    overflow-x: hidden;
+  }
+
+  div.background,
+  canvas {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+
+  div.background {
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+
+  canvas {
+    cursor: pointer;
+  }
+
+  .pixelated {
+    image-rendering: crisp-edges;
+    image-rendering: pixelated;
+  }
+</style>
