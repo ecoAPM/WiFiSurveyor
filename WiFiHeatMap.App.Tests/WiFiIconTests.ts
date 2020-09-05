2a4c076b8ae34ecc8bea60460c9b5461beff938a
Steve Desmond <steve@vtsv.ca>
2020-09-05 11:17:41 -0400

    Add fading effect to stale Wi-Fi signal

--- WiFiHeatMap.App.Tests/WiFiIconTests.ts
+++ WiFiHeatMap.App.Tests/WiFiIconTests.ts
@@ -15,4 +15,34 @@ export default class WiFiIconTests extends TestSuite {
         //assert
         this.assert.stringContains('style="fill: rgba(0, 255, 0, 1);"', component.html());
     }
+
+    @Test()
+    async colorIsFullByDefault() {
+        //arrange
+        const color = 'rgba(0, 255, 0, 1)';
+
+        //act
+        const component = mount(WiFiIcon, { propsData: { color: color } });
+        await component.vm.$nextTick();
+        
+        //assert
+        this.assert.stringDoesNotContain('fading', component.find('svg').html());
+    }
+
+    @Test()
+    async colorFadesAsReadingGoesStale() {
+        //arrange
+        const color = 'rgba(0, 255, 0, 1)';
+        const component = mount(WiFiIcon, { propsData: { color: color } });
+        await component.vm.$nextTick();
+
+        //act
+        component.setProps({color: 'rgba(255, 255, 0, 1)'});
+        await component.vm.$nextTick();
+        await new Promise((resolve) => setTimeout(() => resolve(), 100));
+        await component.vm.$nextTick();
+        
+        //assert
+        this.assert.stringContains('fading', component.find('svg').html());
+    }
 }
