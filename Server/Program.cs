4d928d3a198629b55eae174ecc7c25ebcfe2eefe
Steve Desmond <steve@vtsv.ca>
2021-11-30 18:11:07 -0500

    Randomize port on startup

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.Extensions.Hosting;
 
 namespace WiFiSurveyor;
@@ -8,9 +13,17 @@ public static class Program
 {
 	public static async Task Main(string[] args)
 	{
-		await Host.CreateDefaultBuilder(args)
-			.ConfigureWebHostDefaults(builder => builder.UseStartup<Startup>())
-			.Build()
-			.RunAsync();
+		var host = WebHost.CreateDefaultBuilder()
+			.UseStartup<Startup>()
+			.UseUrls("http://127.0.0.1:0")
+			.Build();
+
+		host.Start();
+
+		var binding = host.ServerFeatures.Get<IServerAddressesFeature>();
+		var address = binding.Addresses.FirstOrDefault();
+		Process.Start(address);
+
+		await host.WaitForShutdownAsync();
 	}
 }
