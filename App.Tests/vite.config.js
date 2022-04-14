062814f231ed568c406eb932af79b7079a470edb
Steve Desmond <Steve@ecoAPM.com>
2022-04-14 16:49:13 -0400

    Fix global JSDOM registration

--- App.Tests/vite.config.js
+++ App.Tests/vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: files,
 			output: {
-				intro: "require('global-jsdom')();"
+				intro: "require('global-jsdom/register');"
 			},
 			external: ["xunit.ts", "ts-mockito", "vue", "@vue/test-utils"]
 		}
