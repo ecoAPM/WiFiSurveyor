5152a0d52126b55348f0305681a7f43f93a9976a
Steve Desmond <Steve@ecoAPM.com>
2022-12-04 10:35:13 -0500

    Handle multi-byte SSID characters in signal parsers

--- Mac/MacSignalParser.cs
+++ Mac/MacSignalParser.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using WiFiSurveyor.Core;
 
 namespace WiFiSurveyor.Mac;
@@ -12,27 +13,27 @@ public sealed class MacSignalParser : ISignalParser<string>
 	public IReadOnlyList<Signal> Parse(string results)
 		=> results.Split("\n", StringSplitOptions.RemoveEmptyEntries)
 			.Skip(1)
-			.Select(GetSignal)
+			.Select(line => GetSignal(Encoding.UTF8.GetBytes(line)))
 			.Where(s => s is not null)
 			.Cast<Signal>()
 			.ToArray();
 
-	private Signal? GetSignal(string line)
+	private Signal? GetSignal(ReadOnlySpan<byte> line)
 	{
 		try
 		{
 			return new()
 			{
-				SSID = line[..32].Trim(),
-				MAC = line[32..50].Trim(),
-				Strength = short.Parse(line[50..55].Trim()),
-				Channel = GetChannel(line[55..64].Trim()),
-				Frequency = GetFrequency(line[55..64].Trim())
+				SSID = Encoding.UTF8.GetString(line[..32]).Trim(),
+				MAC = Encoding.UTF8.GetString(line[32..50]).Trim(),
+				Strength = short.Parse(Encoding.UTF8.GetString(line[50..55]).Trim()),
+				Channel = GetChannel(Encoding.UTF8.GetString(line[55..64]).Trim()),
+				Frequency = GetFrequency(Encoding.UTF8.GetString(line[55..64]).Trim())
 			};
 		}
 		catch (Exception)
 		{
-			_logger.LogIf(LogLevel.Warning, "Could not parse signal data: {0}", line);
+			_logger.LogIf(LogLevel.Warning, "Could not parse signal data: {0}", Encoding.UTF8.GetString(line));
 			return null;
 		}
 	}
