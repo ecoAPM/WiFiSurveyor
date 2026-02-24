15409dc040158f43b022ec34a1c8ac8ff1e195bc
Steve Desmond <Steve@ecoAPM.com>
2026-02-24 13:57:11 -0500

    Use single pushes for vertex creation

--- App/Triangulation.ts
+++ App/Triangulation.ts
@@ -2,7 +2,7 @@ import Delaunator from "delaunator";
 import ColorConverter from "./ColorConverter";
 import Reading from "./Reading";
 import AccessPoint from "./AccessPoint";
-import { Mode } from "./Mode";
+import {Mode} from "./Mode";
 
 export default class Triangulation {
 	readonly vertex_coordinates: number[] = [];
@@ -14,8 +14,6 @@ export default class Triangulation {
 
 		delauney.triangles.forEach(index => {
 			const reading = readings[index];
-			this.vertex_coordinates.push(reading.location.x);
-			this.vertex_coordinates.push(reading.location.y);
 
 			const signal = mode == Mode.Signal
 				? reading.signalFor(access_point)
@@ -25,10 +23,18 @@ export default class Triangulation {
 				? ColorConverter.fromSignal(signal)
 				: ColorConverter.fromSNR(signal);
 
-			this.vertex_color_parts.push(color.red);
-			this.vertex_color_parts.push(color.green);
-			this.vertex_color_parts.push(color.blue);
-			this.vertex_color_parts.push((color.alpha > 0 ? color.alpha + 1 : color.alpha) * 0.75 - (color.alpha > 0 ? 1 : 0));
+			this.vertex_coordinates.push(
+				reading.location.x,
+				reading.location.y
+			);
+
+			const alpha = (color.alpha > 0 ? color.alpha + 1 : color.alpha) * 0.75 - (color.alpha > 0 ? 1 : 0);
+			this.vertex_color_parts.push(
+				color.red,
+				color.green,
+				color.blue,
+				alpha
+			);
 		});
 	}
 }
